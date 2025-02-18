f994d0ed28bc00438a5985a75032b900e827deea
Gilberth702 <hgilbert960@boyslatin.net>
2025-02-18 19:24:49 +0000

    complete notes on Py operators

---
+++ unit 1/operators.py
@@ -0,0 +1,37 @@
+# Data manipulation
+# assignment operators
+# comparison operators
+# logical operators
+# arithmetic operators
+#name = # Hamaaj # the equal sign is the operator
+# use some combination of the equal 
+# to give a variable a value 
+# true or false 
+#  comparisons operators allow us to evaluate data
+# based on several factors 
+# 10 less than 20
+# greater than or equal 
+# 100 = 100 
+# 100 not the same as 10
+# aritmatic allouw us to perform math with specifically with the integer and float
+# data types 
+# addition
+# subtraction
+# multiplication
+# division 
+# We use logical operators to evaluate if data is true or false 
+# 10 x 3
+# 100 / 4
+# laptopdell = 1000
+# password = '123' and 4 == mathAnswer
+# securityQuestion = 'Franks'
+# and - used to compare to condtitons that are both true 
+# MathAnswer = 4
+# If both conditions below are true we will have the value 
+# of true
+# or - used to compare conditions, But only one needs to
+# to be true in order for the output to be true
+# not - used to compare conditions, But will
+# output the opposite result meaning if two things are ture
+# we are going to get false
+# whatever the real result is, will become the opposite

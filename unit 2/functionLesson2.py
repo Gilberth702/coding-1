e85b1c7f8b8a04c52cc79b001ab2f11d1db95bce
Gilberth702 <hgilbert960@boyslatin.net>
2025-03-04 19:16:58 +0000

    Completed the functionLesson2.py of unit 2

---
+++ unit 2/functionLesson2.py
@@ -0,0 +1,11 @@
+# create a clone function were deta that is passed in will be copied and then printed out
+def cloneMachine():
+    print('this is the original:')
+    print('this is the clone:)
+      cloneMachine ('Hamaaj')    
+      print('original number is: '+ result)
+      cloneMachine2(4)
+      def email(emailAddress, password)
+      print ('great you have access')
+      print ('you cannot enter')
+      captca == captca:
